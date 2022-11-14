63d34d7595aa41839747059817bee606c5c3f00c
X1K00 <fcju14@gmail.com>
2022-11-14 11:30:46 -0300

    Copiar e colar apenas arquivos, ou pastas com dados dentro

--- main.py
+++ main.py
@@ -6,46 +6,42 @@ class Files:
         self.pasta_atual = os.getcwd()
         self.pastas = os.listdir()
         self.pasta_anterior = os.path.dirname(os.getcwd())
-        self.copia_pastas = []
-        self.copia_arquivo = copia
+        self.area_tranfer = copia
     
     def main(self):
-        self.topo()
-        self.listar_pastas()
-        com = input("R: ")
+        print("----------------------------------------------------------------|\n"
+        "->",os.getcwd(),"\n"
+        "n- criar pasta |d- remover |r- renomear |c- copiar |x- cortar |v- colar\n\n"
+        "0 - Voltar")
 
-        if com.isnumeric():
-            self.com_numero(int(com))  
+        self.listar_pastas()
+        n = input("R: ")
+        if n.isnumeric():
+            self.com_numero(int(n))  
         else:
-            self.com_letra(com)
+            self.com_letra(n)
 
     def listar_pastas(self):
         for n, i in enumerate(self.pastas):
             print(n+1, "-", i)
-
-    def topo(self):
-        print("----------------------------------------------------------------|\n"
-        "->",os.getcwd(),"\n"
-        "n- criar pasta |d- remover |r- renomear |c- copiar |x- cortar \n\n"
-        "0 - Voltar")
         
     def com_numero(self, n):
-        print("numero", n)
         if n == 0:
             os.chdir(self.pasta_anterior)
         else:
-            if "." in self.pastas[n-1]:
+            if os.path.isfile(self.pastas[n-1]):
                 os.startfile(self.pastas[n-1])
             else:
                 os.chdir("{}\{}".format(self.pasta_atual, self.pastas[n-1]))
-        self.__init__(self.copia_arquivo)
+
+        self.__init__(self.area_tranfer)
         self.main()
 
     def com_letra(self, l):
-        copia = self.copia_arquivo
+        copia = self.area_tranfer
 
         if l == "n":
-            os.mkdir(input("Nome da pasta: "))
+            os.mkdir(input("Nome da nova pasta: "))
             print("Pasta criada com sucesso!!")
 
         elif l == "c":
@@ -54,27 +50,21 @@ class Files:
             self.listar_pastas()
             n = int(input("Copiar: "))
             copia = []
-            copia_pastas = []
 
-            if "." in self.pastas[n-1]:
-                print("Copiando:", self.pastas[n-1])
-                copia.append("{}\{}".format(self.pasta_atual, self.pastas[n-1]))
-                copia.append(self.pastas[n-1])
-                print(self.copia_arquivo)
-            else:
-                for root, dirs, files in os.walk("{}\{}".format(self.pasta_atual, self.pastas[n-1])):
-                    for fim in files:
-                        print("Copiando: {}\{}".format(root,fim))
-                        #os.remove("{}\{}".format(root,fim))
-                    else:
-                        print("Copiando:", root)
-                        #os.rmdir(root)
+            print("Copiando:", self.pastas[n-1])
+            copia.append("{}\{}".format(self.pasta_atual, self.pastas[n-1]))
+            copia.append(self.pastas[n-1])
+
         elif l == "v":
-            print(self.copia_arquivo)
-            print("Origem", self.copia_arquivo[0])
-            print("Destino", "{}\{}".format(self.pasta_atual, self.copia_arquivo[1]))
-            shutil.copy2(self.copia_arquivo[0], "{}\{}".format(self.pasta_atual, self.copia_arquivo[1]), topdown=False)
-            print("Copia pronta!")
+            print("Nome do arquivo:", self.area_tranfer[1], "É arquivo:", os.path.isfile(self.area_tranfer[0]))
+            print("Origem", self.area_tranfer[0])
+            print("Destino", "{}\{}".format(self.pasta_atual, self.area_tranfer[1]))
+
+            if os.path.isfile(self.area_tranfer[0]):
+                shutil.copy2(self.area_tranfer[0], "{}\{}".format(self.pasta_atual, self.area_tranfer[1]))
+            else:
+                shutil.copytree(self.area_tranfer[0], "{}\{}".format(self.pasta_atual, self.area_tranfer[1]))
+            print("Copiado com sucesso!!")
 
         elif l == "r":
             print("----------------------------------------------------------------|\n"
@@ -89,7 +79,8 @@ class Files:
             "REMOVER ARQUIVO\n")
             self.listar_pastas()
             n = int(input("Remover: "))
-            if "." in self.pastas[n-1]:
+
+            if os.path.isfile(self.pastas[n-1]):
                 print("Removendo:", self.pastas[n-1])
                 os.remove("{}\{}".format(self.pasta_atual, self.pastas[n-1]))
             else:
@@ -105,8 +96,7 @@ class Files:
 
         self.__init__(copia)
         self.main()
-
-         
+       
 arquivo = Files([])
 arquivo.main()
     
