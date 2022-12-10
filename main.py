10b0749d725439737707793f33d341e6415d4e1f
X1K00 <fcju14@gmail.com>
2022-12-10 09:56:31 -0300

    Tratamento de exceções e melhorias

--- main.py
+++ main.py
@@ -62,20 +62,23 @@ class Files:
             print(n+1, "-", i)  
     
     def navegacao(self, n): #Todos os comandos referentes a navegação estão aqui
-        if (n == "0") or (n == "Voltar"):
-            os.chdir(self.pasta_anterior) # 0 retona uma pasta no diretorio 
-        elif n.isnumeric(): #Qualquer outro numero além de o 0 entra em uma pasta ou abre um arquivo correspondente ao numero digitado
-            n = int(n)
-            if os.path.isfile(self.pastas[n-1]):  
-                os.startfile(self.pastas[n-1]) # Abre arquivo
+        try:    
+            if (n == "0") or (n == "Voltar"):
+                os.chdir(self.pasta_anterior) # 0 retona uma pasta no diretorio 
+            elif n.isnumeric(): #Qualquer outro numero além de o 0 entra em uma pasta ou abre um arquivo correspondente ao numero digitado
+                n = int(n)
+                if os.path.isfile(self.pastas[n-1]):  
+                    os.startfile(self.pastas[n-1]) # Abre arquivo
+                else:
+                    os.chdir(os.path.join(self.pasta_atual, self.pastas[n-1])) #Entra na pasta
             else:
-                os.chdir(os.path.join(self.pasta_atual, self.pastas[n-1])) #Entra na pasta
-        else:
-            if os.path.isfile(n):  
-                os.startfile(n) # Abre arquivo
-            else:
-                os.chdir(os.path.join(self.pasta_atual, n))
-        
+                if os.path.isfile(n):  
+                    os.startfile(n) # Abre arquivo
+                else:
+                    os.chdir(os.path.join(self.pasta_atual, n))
+        except IndexError:
+            print("\n\nPasta ou arquivo não encontrado!!")
+    
     def apagar(self, end, n): # Apaga os arquivos ou pastas
         if os.path.isfile(n): #Apaga arquivos
             print("Removendo:", n)
@@ -105,7 +108,6 @@ class Files:
             copia.append(n) # Adiciona o endereço e o nome do objeto escolhido a uma lista que será salva
             copia.append(t)
             return copia
-
 
     def atalho(self, p, a): # Função que cria links simbolicos
         print("----------------------------------------------------------------------|\n"
@@ -139,153 +141,194 @@ class Files:
             except OSError:
                 print("\nVocê precisa acessar essa função como administrador!!\n")
 
-    def comandos(self, l): # Todos os comandos menos os de navegação estão aqui
+    def comandos(self, n): # Todos os comandos menos os de navegação estão aqui
         copia = self.area_tranfer
+        l = n.lower()
 
-        if l == "s": # "s" para sair do sistema
-            print("Saindo do sistema...")
-            exit()
+        try:
+            if l == "s": # "s" para sair do sistema
+                print("Saindo do sistema...")
+                exit()
 
-        elif l == "x": # "x" para cortar um arquivo ou pasta
-            print("----------------------------------------------------------------------|\n"
-            "CORTAR\n")
-            self.listar_pastas()
-            n = input("Cortar: ")
+            elif l == "x": # "x" para cortar um arquivo ou pasta
+                print("----------------------------------------------------------------------|\n"
+                "CORTAR\n")
+                self.listar_pastas()
+                n = input("Cortar: ")
 
-            if n == "0" or (n == "Voltar"):
-                pass
-            elif n.isnumeric():
-                copia = self.copiar(n, "x")
-            else:
-                if os.path.exists(n):
+                if n == "0" or (n == "Voltar"):
+                    pass
+                elif n.isnumeric():
                     copia = self.copiar(n, "x")
                 else:
-                    print("\n\nArquivo ou pasta inexistente!!")
+                    if os.path.exists(n):
+                        copia = self.copiar(n, "x")
+                    else:
+                        print("\n\nArquivo ou pasta inexistente!!")
 
-        elif l == "n": # "n" cria uma nova pasta
-            os.mkdir(input("Nome da nova pasta: "))
-            print("Pasta criada com sucesso!!")
+            elif l == "n": # "n" cria uma nova pasta
+                os.mkdir(input("Nome da nova pasta: "))
+                print("Pasta criada com sucesso!!")
 
-        elif l == "c": # "c" faz a copia do arquivo ou da pasta
-            print("----------------------------------------------------------------------|\n"
-            "COPIAR\n")
-            self.listar_pastas()
-            n = input("Copiar: ")
+            elif l == "c": # "c" faz a copia do arquivo ou da pasta
+                print("----------------------------------------------------------------------|\n"
+                "COPIAR\n")
+                self.listar_pastas()
+                n = input("Copiar: ")
 
-            if n == "0" or (n == "Voltar"):
-                pass
-            elif n.isnumeric():
-                copia = self.copiar(n, "c")
-            else:
-                if os.path.exists(n):
+                if n == "0" or (n == "Voltar"):
+                    pass
+                elif n.isnumeric():
                     copia = self.copiar(n, "c")
                 else:
-                    print("\n\nArquivo ou pasta inexistente!!")
+                    if os.path.exists(n):
+                        copia = self.copiar(n, "c")
+                    else:
+                        print("\n\nArquivo ou pasta inexistente!!")
 
-        elif l == "l": # "l" para criar um link
-            print("----------------------------------------------------------------------|\n"
-            "ATALHO\n")
-            self.listar_pastas()
-            n = input("Atalho de: ")
+            elif l == "l": # "l" para criar um link
+                print("----------------------------------------------------------------------|\n"
+                "ATALHO\n")
+                self.listar_pastas()
+                n = input("Atalho de: ")
 
-            if n == "0" or (n == "Voltar"):
-                pass
-            elif n.isnumeric():
-                n = int(n)
-                path = self.pasta_atual
-                arq = self.pastas[n-1]
-                self.atalho(path, arq)
-            else:
-                if os.path.exists(n):
+                if n == "0" or (n == "Voltar"):
+                    pass
+                elif n.isnumeric():
+                    n = int(n)
                     path = self.pasta_atual
-                    arq = n
+                    arq = self.pastas[n-1]
                     self.atalho(path, arq)
                 else:
-                    print("\n\nArquivo ou pasta inexistente!!")
+                    if os.path.exists(n):
+                        path = self.pasta_atual
+                        arq = n
+                        self.atalho(path, arq)
+                    else:
+                        print("\n\nArquivo ou pasta inexistente!!")
+                
+            elif l == "v": # "v" para colar os arquivos ou pastas cortadas ou copiadas
+                orig = os.path.join(self.area_tranfer[0], self.area_tranfer[1])
+                dest = os.path.join(self.pasta_atual, self.area_tranfer[1])
+
+                if os.path.isfile(orig):
+                    shutil.copy2(orig, dest)
+                else:
+                    shutil.copytree(orig, dest)
+                if self.area_tranfer[2] == "c":
+                    print("Copiado com sucesso!!")
+                else:
+                    self.apagar(self.area_tranfer[0], self.area_tranfer[1])
+                    print("Cortado com sucesso!!")
+
+            elif l == "r": # "r" renomeia o arquivo ou a pasta selecionado
+                print("----------------------------------------------------------------------|\n"
+                "RENOMEAR\n")
+                self.listar_pastas()
+                n = input("Renomear: ")
+
+                try:
+                    if n == "0" or (n == "Voltar"):
+                        pass
+                    elif n.isnumeric():
+                        n = int(n)
+                        print("Renomear:",self.pastas[n-1])
+                        novo_nome = input("Novo nome: ")
+                        os.rename(self.pastas[n-1], novo_nome)
+                    else:
+                        if os.path.exists(n):
+                            novo_nome = input("Novo nome: ")
+                            os.rename(n, novo_nome)
+                        else:
+                            print("\n\nArquivo ou pasta inexistente!!")
+                except FileExistsError:
+                    print("\n\nNão é possível criar um arquivo já existente!!")
             
-        elif l == "v": # "v" para colar os arquivos ou pastas cortadas ou copiadas
-            orig = os.path.join(self.area_tranfer[0], self.area_tranfer[1])
-            dest = os.path.join(self.pasta_atual, self.area_tranfer[1])
-            print("Origem", orig)
-            print("Destino", dest)
+            elif l == "d": # "d" apaga o arquivou ou pasta selecionada 
+                print("----------------------------------------------------------------------|\n"
+                "REMOVER\n")
+                self.listar_pastas()
+                n = input("Remover: ")
 
-            if os.path.isfile(orig):
-                shutil.copy2(orig, dest)
-            else:
-                shutil.copytree(orig, dest)
-            if self.area_tranfer[2] == "c":
-                print("Copiado com sucesso!!")
-            else:
-                self.apagar(self.area_tranfer[0], self.area_tranfer[1])
-                print("Cortado com sucesso!!")
-
-        elif l == "r": # "r" renomeia o arquivo ou a pasta selecionado
-            print("----------------------------------------------------------------------|\n"
-            "RENOMEAR\n")
-            self.listar_pastas()
-            n = int(input("Renomear: "))
-            novo_nome = input("Novo nome: ")
-            os.rename(self.pastas[n-1], novo_nome)
-        
-        elif l == "d": # "d" apaga o arquivou ou pasta selecionada 
-            print("----------------------------------------------------------------------|\n"
-            "REMOVER\n")
-            self.listar_pastas()
-            n = int(input("Remover: "))
-            end = self.pasta_atual
-            path = self.pastas[n-1]
-
-            self.apagar(end, path)          
-            print("Remoção com sucesso!!")
-        
-        elif l == "i": # "i" para saber informações sobre determinada pasta ou arquivo
-            print("----------------------------------------------------------------------|\n"
-            "INFORMAÇÕES\n")
-            self.listar_pastas()
-            n = int(input("Informações: "))
-
-            path = self.pastas[n-1]
-            t_criacao = os.path.getctime(path)
-            t_modific = os.path.getmtime(path)
-            t_acess = os.path.getatime(path)
-            tamanho = os.path.getsize(path)
-            proprie = os.stat(path).st_uid
-            grupo = os.stat(path).st_gid
-            p_ler = os.access(path, os.R_OK)
-            p_gra = os.access(path, os.W_OK)
-            p_exe = os.access(path, os.X_OK)
-
-            while True:
+                if n == "0" or (n == "Voltar"):
+                    pass
+                elif n.isnumeric():
+                    n = int(n)
+                    self.apagar(self.pasta_atual, self.pastas[n-1])          
+                    print("Removido com sucesso!!")
+                else:
+                    if os.path.exists(n):
+                        self.apagar(self.pasta_atual, n)          
+                        print("Removido com sucesso!!")
+                    else:
+                        print("\n\nArquivo ou pasta inexistente!!")
+            
+            elif l == "i": # "i" para saber informações sobre determinada pasta ou arquivo
                 print("----------------------------------------------------------------------|\n"
                 "INFORMAÇÕES\n")
-                print("Nome:",path)
-                print("IP Proprietario:", proprie)
-                print("IP Grupo:", grupo)
-                print(time.strftime("Criado em: %d/%m/%Y ás %H:%M", time.gmtime(t_criacao)))
-                print(time.strftime("Modificado em: %d/%m/%Y ás %H:%M", time.gmtime(t_modific)))
-                print(time.strftime("Acessado em: %d/%m/%Y ás %H:%M", time.gmtime(t_acess)))
-                print("Tamanho: {} KB ({} bytes)".format(round(tamanho/1000), tamanho))
-                print("Permições: Ler: {} | Gravar: {} | Executar: {}".format(p_ler, p_gra, p_exe))
-                print()
-                acao = input("0 - Voltar\n1 - Alterar Proprietario\n2 - Alterar Grupo\n3 - Alterar Acessos\nR: ")
-                if acao == "0":
-                    break
-                elif acao == "1":
-                    pro = input("Novo Proprietario: ")
-                    print("Não está disponivel nesse sistema operacional")
-                    #os.fchown(path, pro, -1)
-                elif acao == "2":
-                    gru = input("Novo Grupo: ")
-                    print("Não está disponivel nesse sistema operacional")
-                    #os.fchown(path, -1, gru)
-                elif acao == "3":
-                    print("Não está disponivel nesse sistema operacional")
+                self.listar_pastas()
+                n = input("Informações: ")
+                path = None
 
-        else:
-            print("\n\nComando inexistente!!")   
+                if n == "0" or (n == "Voltar"):
+                    pass
+                elif n.isnumeric():
+                    n = int(n)
+                    path = self.pastas[n-1]
+                else:
+                    if os.path.exists(n):
+                        path = n
+                    else:
+                        print("\n\nArquivo ou pasta inexistente!!")
+
+                if path == None:
+                    pass
+                else:
+                    t_criacao = os.path.getctime(path)
+                    t_modific = os.path.getmtime(path)
+                    t_acess = os.path.getatime(path)
+                    tamanho = os.path.getsize(path)
+                    proprie = os.stat(path).st_uid
+                    grupo = os.stat(path).st_gid
+                    p_ler = os.access(path, os.R_OK)
+                    p_gra = os.access(path, os.W_OK)
+                    p_exe = os.access(path, os.X_OK)
+
+                    while True:
+                        print("----------------------------------------------------------------------|\n"
+                        "INFORMAÇÕES\n")
+                        print("Nome:",path)
+                        print("IP Proprietario:", proprie)
+                        print("IP Grupo:", grupo)
+                        print(time.strftime("Criado em: %d/%m/%Y ás %H:%M", time.gmtime(t_criacao)))
+                        print(time.strftime("Modificado em: %d/%m/%Y ás %H:%M", time.gmtime(t_modific)))
+                        print(time.strftime("Acessado em: %d/%m/%Y ás %H:%M", time.gmtime(t_acess)))
+                        print("Tamanho: {} KB ({} bytes)".format(round(tamanho/1000), tamanho))
+                        print("Permições: Ler: {} | Gravar: {} | Executar: {}".format(p_ler, p_gra, p_exe))
+                        print()
+                        acao = input("0 - Voltar\n1 - Alterar Proprietario\n2 - Alterar Grupo\n3 - Alterar Acessos\nR: ")
+                        
+                        if acao == "0":
+                            break
+                        
+                        elif acao == "1":
+                            pro = input("Novo Proprietario: ")
+                            print("Não está disponivel nesse sistema operacional")
+                            #os.fchown(path, pro, -1)
+                        elif acao == "2":
+                            gru = input("Novo Grupo: ")
+                            print("Não está disponivel nesse sistema operacional")
+                            #os.fchown(path, -1, gru)
+                        elif acao == "3":
+                            print("Não está disponivel nesse sistema operacional")
+
+            else:
+                print("\n\nComando inexistente!!")
+        except IndexError:
+            print("\n\nPasta ou arquivo não encontrado!!")   
         
         self.__init__(copia)
         self.main()
-       
+
 arquivo = Files(["","",""])
 arquivo.main()
